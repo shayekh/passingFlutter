0ee9e7ef868ce50bca0a42af8a602f1fb41ec49d
shayekh <shayekhebnemizan@gmail.com>
2021-02-19 19:48:54 +0600

    Passing

--- lib/stwo.dart
+++ lib/stwo.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 
-class Stwo extends StatelessWidget {
+// class Stwo extends StatelessWidget {
+//   String value;
+//   Stwo({this.value});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//             "S2"
+//         ),
+//       ),
+//       body: Center(
+//         child: Text(
+//             value,
+//         ),
+//       ),
+//     );
+//   }
+// }
+class Stwo extends StatefulWidget {
+
   String value;
-  Stwo({this.value});
+   Stwo({this.value});
+  @override
+  _StwoState createState() => _StwoState(value);
+}
+
+class _StwoState extends State<Stwo> {
+  String value;
+  _StwoState(this.value);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
